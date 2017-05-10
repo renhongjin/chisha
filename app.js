7fa8421fd744b9ea898e1bcf55c9f3ac86d731f7
ren hongjin <renhongjin@danlu.com>
2017-05-10 19:10:36 +0800

    新增d导航

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 //app.js
 App({
   onLaunch: function (options) {
+    //获取场景值与打开小程序的路径
+    var scene = options.scene;
+    var path = options.path;
+
+    console.log("场景值：" + scene)
     console.log(options)
+
     //调用API从本地缓存中获取数据
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+
+    
   },
   getUserInfo:function(cb){
     var that = this
